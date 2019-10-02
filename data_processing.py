c30a3445cdea22639fe709b650499899a9af7fd0
Ian Schenck <ianschenck123@gmail.com>
2019-10-02 11:36:27 -0700

    working plotly bubble chart using random dilation and event data

--- data_processing.py
+++ data_processing.py
@@ -1,53 +1,93 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import plotly.graph_objects as go
+import plotly.express as px
+import math
 
 def load_file(path, names):
     if not path.is_file():
         raise FileNotFoundError(str(path))
         
-    data = pd.read_csv(file, sep="\t", names=names, header=None)
+    data = pd.read_csv(path, sep="\t", names=names, header=None)
     return data
 
-EVD_cols = ["time", "event", "event_key", "data_1", "data_2", "description"]
-FXD_cols = ["fix_number", "timestamp", "duration", "gazepoint_x", "gazepoint_y"]
-GZD_cols = ["gazepoint_X_L" ,"gazepoint_Y_L", "cam_X_L", "cam_Y_L",
+def load_datasets():
+    EVD_cols = ["time", "event", "event_key", "data_1", "data_2", "description"]
+    FXD_cols = ["fix_number", "timestamp", "duration", "gazepoint_x", "gazepoint_y"]
+    GZD_cols = ["gazepoint_X_L" ,"gazepoint_Y_L", "cam_X_L", "cam_Y_L",
                 "distance_L", "pupil_L", "validity_L", "gazepoint_X_R",
                 "gazepoint_Y_R", "cam_X_R", "cam_Y_R", "distance_R", "pupil_R", 
                  "validity_R"]
+    path = Path.cwd() / "p4"
+    graphEVD = load_file(path / "p4.graphEVD.txt", EVD_cols)
+    graphFXD = load_file(path / "p4.graphFXD.txt", FXD_cols)
+    graphGZD = load_file(path / "p4.graphGZD.txt", GZD_cols)
+    treeEVD = load_file(path / "p4.treeEVD.txt", EVD_cols)
+    treeFXD = load_file(path / "p4.treeFXD.txt", FXD_cols)
+    treeGZD = load_file(path / "p4.treeGZD.txt", GZD_cols)
+    GZD = load_file(path /"p4GZD.txt", GZD_cols)
+    return graphEVD, graphFXD, graphGZD, treeEVD, treeFXD, treeGZD, GZD
 
-path = Path.cwd() / "p4"
+def remove_invalid_data(GZD):
+    indexNames = GZD[(GZD['validity_L'] > 0) | (GZD['validity_R'] > 0)].index
+    GZD.drop(indexNames, inplace=True)
+    return GZD
 
-file = path / "p4.graphEVD.txt"
-graphEVD = load_file(file, EVD_cols)
+def calculate_saccade_length(FXD):
+    x = np.square(FXD[['gazepoint_x', 'gazepoint_y']].diff())
+    x = np.sqrt(x['gazepoint_x'] + x['gazepoint_y'])
+    x = x.fillna(0.)
+    return x
 
-file = path / "p4.graphFXD.txt"
-graphFXD = load_file(file, FXD_cols)
+graphEVD, graphFXD, graphGZD, treeEVD, treeFXD, treeGZD, GZD = load_datasets()
 
-file = path / "p4.graphGZD.txt"
-graphGZD = load_file(file, GZD_cols)
+graphGZD = remove_invalid_data(graphGZD)
+treeGZD = remove_invalid_data(treeGZD)
+GZD = remove_invalid_data(GZD)
 
-file = path / "p4.treeEVD.txt"
-treeEVD = load_file(file, EVD_cols)
+graphFXD['saccade_length'] = calculate_saccade_length(graphFXD)
 
-file = path / "p4.treeFXD.txt"
-treeFXD = load_file(file, FXD_cols)
+treeFXD['saccade_length'] = calculate_saccade_length(treeFXD)
 
-file = path / "p4.treeGZD.txt"
-treeGZD = load_file(file, GZD_cols)
+#randomized event and dilation for now
+event_names = ['LMouseButton', 'Keyboard', 'RMouseButton']
+graphFXD['event'] = np.random.choice(event_names, size=len(graphFXD))
+graphFXD['dilation'] = np.random.choice(10000, size=len(graphFXD))
 
-file = path /"p4GZD.txt"
-GZD = load_file(file, GZD_cols)
+hover_text = []
+bubble_size = []
 
+for index, row in graphFXD.iterrows():
+    hover_text.append(('Fixation Duration: {duration}<br>'+
+                        'Saccade Length: {saccade_length}<br>')
+                        .format(duration=row['duration'], 
+                        saccade_length=row['saccade_length']))
+    bubble_size.append(math.sqrt(row['dilation']))
 
-indexNames = graphGZD[(graphGZD['validity_L'] > 0) | (graphGZD['validity_R'] > 0)].index
-graphGZD.drop(indexNames, inplace=True)
-print(graphGZD)
+graphFXD['text'] = hover_text
+graphFXD['size'] = bubble_size
+sizeref = 2.*max(graphFXD['size'])/(100**2)
 
-indexNames = treeGZD[(treeGZD['validity_L'] > 0) | (treeGZD['validity_R'] > 0)].index
-treeGZD.drop(indexNames, inplace=True)
-print(treeGZD)
+event_data = {event:graphFXD.query("event == '%s'" %event) for event in event_names}
 
-indexNames = GZD[(GZD['validity_L'] > 0) | (GZD['validity_R'] > 0)].index
-GZD.drop(indexNames, inplace=True)
-print(GZD)
+fig = go.Figure() 
+
+for event_name, event in event_data.items():
+    fig.add_trace(go.Scatter(
+        x=event['duration'], y=event['saccade_length'],
+        name=event_name, text = event['text'],
+        marker_size=event['size']
+    ))
+
+fig.update_traces(mode='markers', marker=dict(sizemode='area', sizeref=sizeref, line_width=2))
+
+fig.update_layout(
+    title='Fixation Duration v. Saccade Length',
+    xaxis=dict(title='Fixation Duration', gridcolor='white', gridwidth=2,),
+    yaxis=dict(title='Saccade Length', gridcolor='white', gridwidth=2,),
+    paper_bgcolor='rgb(243, 243, 243)',
+    plot_bgcolor='rgb(243, 243, 243)',
+)
+
+fig.show()
